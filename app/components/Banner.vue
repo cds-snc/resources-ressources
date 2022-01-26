79944982101b19c3605851fde35a7771d45701da
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-01-26 15:58:15 -0800

    chore: design, images, and more components

---
+++ app/components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Proof of concept banner -->
+  <div class="bg-white">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-start flex-wrap pt-4 pb-4">
+          <div class="bg-cds-blue text-white p-2 pl-4 pr-4 uppercase">
+            Proof of Concept
+          </div>
+          <p class="p-2">This content is a work in progress. 
+            <Link to="http://google.com" :external=true>Help us improve.</Link></p>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Link from '~/components/Link.vue'
+
+export default {
+  components: { 
+    Link
+  },
+}
+</script>
---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+        'cds-yellow': '#ffcc33', // CDS yellow,
+        'cds-blue': '#004986'
+        }
+      },
+    },
+  }
+  
