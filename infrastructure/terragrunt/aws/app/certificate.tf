80e9227ab5de1e459d57ea0ca74cccf0cf011dd4
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-03-14 14:47:49 -0700

    ci: tf certificate

--- infrastructure/terragrunt/aws/app/certificate.tf
+++ infrastructure/terragrunt/aws/app/certificate.tf
@@ -1,5 +1,6 @@
 resource "aws_acm_certificate" "app" {
   domain_name       = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method = "DNS"
 
   tags = {
@@ -9,4 +10,28 @@ resource "aws_acm_certificate" "app" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_route53_record" "lr_dns_validation" {
+  zone_id = var.hosted_zone_id
+
+  for_each = {
+    for dvo in aws_acm_certificate.app.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+
+  ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "lr_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.app.arn
+  validation_record_fqdns = [for record in aws_route53_record.app : record.fqdn]
 }
