d8d0db98542edbf03cac2c9a470543f0fc3e1148
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-03-18 12:03:23 -0700

    Feature/infra (#20)
    * ci: tf certificate change
    
    * ci: change sans for tf certificate

--- infrastructure/terragrunt/aws/app/certificate.tf
+++ infrastructure/terragrunt/aws/app/certificate.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "app" {
   domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}", "app.${var.domain_name}", "*.app.${var.domain_name}"]
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
 
   tags = {
