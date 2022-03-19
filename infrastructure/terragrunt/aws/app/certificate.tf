7dfa85373de1b7a811e54e8163198ec469ead2c8
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-03-19 10:38:54 -0700

    bug: fix invalid name in terraform certificate file

--- infrastructure/terragrunt/aws/app/certificate.tf
+++ infrastructure/terragrunt/aws/app/certificate.tf
@@ -66,7 +66,7 @@ resource "aws_route53_record" "validation_app" {
 
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation_main : record.fqdn]
 }
 
 resource "aws_acm_certificate_validation" "app" {
