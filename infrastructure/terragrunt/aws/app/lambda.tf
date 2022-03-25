05d2883d5882776132a9f95f7c7605aef1bf9ec8
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-03-25 15:03:51 -0700

    ci: increase lambda timeout to 5 mins

--- infrastructure/terragrunt/aws/app/lambda.tf
+++ infrastructure/terragrunt/aws/app/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "learning_resources" {
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.learning_resources.repository_url}:latest"
   function_name = "learning-frontend"
-  timeout       = 30
+  timeout       = 300 # 5 minutes
   role          = aws_iam_role.app.arn
 
   environment {
