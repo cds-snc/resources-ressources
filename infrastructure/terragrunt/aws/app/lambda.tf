e88a7e727df05583c41ed8423093e831613a2c54
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-03-25 15:07:10 -0700

    ci: remove comment

--- infrastructure/terragrunt/aws/app/lambda.tf
+++ infrastructure/terragrunt/aws/app/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "learning_resources" {
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.learning_resources.repository_url}:latest"
   function_name = "learning-frontend"
-  timeout       = 300 # 5 minutes
+  timeout       = 300
   role          = aws_iam_role.app.arn
 
   environment {
