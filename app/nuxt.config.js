66682c19a5c588306b5798e63049bd59c2880efc
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2022-06-22 16:02:51 -0400

    Fix page title

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Resources | Resourcess',
+    title: 'Resources | Ressources',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
