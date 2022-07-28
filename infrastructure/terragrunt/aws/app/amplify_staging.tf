db8a7b9ddd15760e017898a0f0f092957ff14e53
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-07-28 10:47:49 -0700

    Add staging environment (#158) (#160)
    * ci: add staging environement on amplify, modify minor prod settings (names)
    
    * ci: update amplify values
    
    * ci: fix for invalid value for stage
    
    * ci: linting

--- infrastructure/terragrunt/aws/app/amplify_staging.tf
+++ infrastructure/terragrunt/aws/app/amplify_staging.tf
@@ -1,5 +1,5 @@
-resource "aws_amplify_app" "learning_resources" {
-  name       = "learning-app"
+resource "aws_amplify_app" "learning_resources_staging" {
+  name       = "Learning Resources (Staging)"
   repository = "https://github.com/cds-snc/resources-ressources"
 
   # Github personal access token
@@ -36,10 +36,10 @@ resource "aws_amplify_app" "learning_resources" {
 
   environment_variables = {
     ENV                         = "staging"
-    DOMAIN_EN                   = "en.learning-resources.cdssandbox.xyz"
-    DOMAIN_FR                   = "fr.learning-resources.cdssandbox.xyz"
+    DOMAIN_EN                   = "en.staging.learning-resources.cdssandbox.xyz"
+    DOMAIN_FR                   = "fr.staging.learning-resources.cdssandbox.xyz"
     contentful_cda_access_token = var.contentful_cda_access_token
-    GOOGLE_ANALYTICS_ID         = var.google_analytics_id
+    GOOGLE_ANALYTICS_ID         = "" # Do not collect data
   }
 
 
@@ -58,53 +58,53 @@ resource "aws_amplify_app" "learning_resources" {
     # Enable auto build for the created branch.
     enable_auto_build             = true
     enable_pull_request_preview   = true
-    pull_request_environment_name = "rc"
-    stage                         = "PRODUCTION"
+    pull_request_environment_name = "pr"
+    stage                         = "PULL_REQUEST"
   }
 }
 
-resource "aws_amplify_branch" "main" {
-  app_id      = aws_amplify_app.learning_resources.id
-  branch_name = "main"
+resource "aws_amplify_branch" "staging" {
+  app_id      = aws_amplify_app.learning_resources_staging.id
+  branch_name = "staging"
 
   framework = "NuxtJS"
 
   # Could be one of: PRODUCTION, BETA, DEVELOPMENT, EXPERIMENTAL, PULL_REQUEST
-  stage = "PRODUCTION"
+  stage = "BETA"
 
-  display_name = "pilot"
+  display_name = "staging"
 
   enable_pull_request_preview = true
 
-  pull_request_environment_name = "RELEASE"
+  pull_request_environment_name = "PULL_REQUEST"
 
   # environment_variables = {
   # }
 }
 
-resource "aws_amplify_domain_association" "learning_resources" {
-  app_id      = aws_amplify_app.learning_resources.id
-  domain_name = "learning-resources.cdssandbox.xyz"
+resource "aws_amplify_domain_association" "learning_resources_staging" {
+  app_id      = aws_amplify_app.learning_resources_staging.id
+  domain_name = "staging.learning-resources.cdssandbox.xyz"
 
   wait_for_verification = false
 
   sub_domain {
-    branch_name = aws_amplify_branch.main.branch_name
+    branch_name = aws_amplify_branch.staging.branch_name
     prefix      = ""
   }
 
   sub_domain {
-    branch_name = aws_amplify_branch.main.branch_name
+    branch_name = aws_amplify_branch.staging.branch_name
     prefix      = "en"
   }
 
   sub_domain {
-    branch_name = aws_amplify_branch.main.branch_name
+    branch_name = aws_amplify_branch.staging.branch_name
     prefix      = "fr"
   }
 
   sub_domain {
-    branch_name = aws_amplify_branch.main.branch_name
+    branch_name = aws_amplify_branch.staging.branch_name
     prefix      = "www"
   }
 }
