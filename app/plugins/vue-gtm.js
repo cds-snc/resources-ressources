0b4ec60a174f38e9316183bb0840f88bf68d27ad
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-08-09 13:34:00 -0700

    feat: add google tag manager

---
+++ app/plugins/vue-gtm.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueGtm from '@gtm-support/vue2-gtm';
+
+/**
+ * @type {import('@nuxt/types').Plugin}
+ */
+export default ({ app, $config: { googleTagManagerID } }) => {
+  Vue.use(
+    VueGtm,
+    {
+      id: googleTagManagerID,
+      vueRouter: app.router
+    }
+  )
+}
