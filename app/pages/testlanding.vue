b57ccfa598acc233d69fa82f4b7a9ce3ae2e42b5
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-09-22 15:43:07 -0700

    feat: add blank landing page for ux testing

---
+++ app/pages/testlanding.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="max-w-5xl mb-10 my-20">
+    Welcome to Learning Resources
+  </div>
+</template>
+
+
+<script>
+export default {
+  layout: 'blank',
+  name: 'TestLanding',
+  async asyncData() {
+  },
+}
+</script>
