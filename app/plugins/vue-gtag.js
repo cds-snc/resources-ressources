f062718b18dfa3f947e97d92143527e453c6d696
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-09-23 15:27:04 -0700

    feat: add gtag option for tracking screenviews

--- app/plugins/vue-gtag.js
+++ app/plugins/vue-gtag.js
@@ -8,8 +8,11 @@ export default ({ app, $config: { googleAnalyticsID } }) => {
   Vue.use(
     VueGtag,
     {
+      pageTrackerScreenviewEnabled: true,
       config: { id: googleAnalyticsID },
     },
+
+    // Auto tracking https://matteo-gabriele.gitbook.io/vue-gtag/auto-tracking
     app.router
   )
 }
