248cfd15e6dc827944c5ab5e7d7f56403cbaff4b
Daine Trinidad <daine.trinidad@cds-snc.ca>
2022-09-23 15:32:40 -0700

    feat: add blank landing page for ux testing (#256)

---
+++ app/layouts/blank.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-grow">
+      <div class="max-w-7xl mx-auto px-4">
+        <Nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'BlankLayout',
+}
+</script>
