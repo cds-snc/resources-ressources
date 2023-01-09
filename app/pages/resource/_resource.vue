e58c38edb9f03f2241c094f137e0925183909be5
Dylan <dylanzheng94@users.noreply.github.com>
2023-01-09 18:42:26 -0500

    refactor: remove commented out rich text render code

--- app/pages/resource/_resource.vue
+++ app/pages/resource/_resource.vue
@@ -14,14 +14,14 @@
           <!-- MVP Feature 3: Content jump links - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
 
           <div
-            v-if="myHeadings.length > 0"
+            v-if="headings.length > 0"
             class="lg:sticky lg:top-40 self-start min-w-1/4 mt-10"
           >
             <h2 class="font-bold text-2xl mb-2.5">{{ $t('jump_to') }}</h2>
             <nav class="jumpLinks">
               <ol>
                 <li
-                  v-for="heading in myHeadings"
+                  v-for="heading in headings"
                   :key="heading.linkId"
                   class="border-l-4 border-solid rounded-r-lg border-gray-200 mx-0 hover:border-blue-700 p-3 hover:bg-blue-50 active:bg-blue-700"
                 >
@@ -74,21 +74,16 @@
 
 <script>
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
-import { BLOCKS, INLINES, MARKS } from '@contentful/rich-text-types'
 import { resourcePageQuery } from '@/utils/queries'
 import { getHeadElement } from '@/utils/headElementAssembler'
-import { getCollectionPath, getTopicPathPrefix } from '@/utils/pathUtility'
-import { ContentTypes } from '@/utils/contentTypes'
 import {richTextRenderOptions} from "@/utils/richTextRenderOptions";
 
-let myHeadings = [];
+let headings = [];
 
 export const myCallback = (heading) => {
   console.log("-- heading: " + heading.linkName)
-  myHeadings.push(heading)
+  headings.push(heading)
 }
-
-
 
 export default {
   layout: 'expandedSearch',
@@ -164,104 +159,14 @@ export default {
       fr: { resource: frRouteParam },
     })
 
-    const headings = []
-
-
-    /* const resourceRichTextRenderOptionsx = (bodyLinks) => {
-      const entryLinks = new Map()
-
-      for (const entry of bodyLinks.entries.hyperlink) {
-        entryLinks.set(entry.sys.id, entry)
-      }
-
-      return {
-        renderMark: {
-          [MARKS.BOLD]: (text) => {
-            return `<strong class="font-bold">${text}</strong>`
-          },
-          [MARKS.ITALIC]: (text) => {
-            return `<i class="italic">${text}</i>`
-          },
-        },
-        renderNode: {
-          [INLINES.HYPERLINK]: (node) => {
-            return `<a class="text-blue-900 underline" href="${node.data.uri}">${node.content[0].value}</a>`
-          },
-          [INLINES.ENTRY_HYPERLINK]: (node) => {
-            const entry = entryLinks.get(node.data.target.sys.id)
-
-            if (entry.__typename === ContentTypes.RESOURCE) {
-              const resourcePath = resourcePathPrefix + entry.urlSlug
-              return `<a class="text-blue-900 underline" href="${resourcePath}">${node.content[0].value}</a>`
-            }
-            if (entry.__typename === ContentTypes.TOPIC) {
-              const topicPath =
-                getTopicPathPrefix(currentLocale) + entry.urlSlug
-              return `<a class="text-blue-900 underline" href="${topicPath}">${node.content[0].value}</a>`
-            }
-            if (entry.__typename === ContentTypes.COLLECTION) {
-              const collectionPath = getCollectionPath(entry.urlSlug)
-              return `<a class="text-blue-900 underline" href="${collectionPath}">${node.content[0].value}</a>`
-            }
-          },
-          [BLOCKS.HEADING_1]: (node) => {
-            return `<h1 class="text-3xl font-medium mt-12 mb-2.5" >${node.content[0].value}</h1>`
-          },
-          [BLOCKS.HEADING_2]: (node) => {
-            const heading = node.content[0].value
-            const headingId = heading.replace(/\s+/g, '-').toLowerCase()
-            headings.push({ linkName: heading, linkId: headingId })
-            return `<h2 id="${headingId}" class="text-3xl font-medium mt-12 mb-2.5 scroll-mt-40">${node.content[0].value}</h2>`
-          },
-          [BLOCKS.HEADING_3]: (node) => {
-            return `<h3 class="text-2xl font-medium mt-12 mb-2.5">${node.content[0].value}</h3>`
-          },
-          [BLOCKS.HEADING_4]: (node) => {
-            return `<h4 class="text-xl font-medium mt-12 mb-2.5">${node.content[0].value}</h4>`
-          },
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          [BLOCKS.PARAGRAPH]: (node, next) => {
-            // return `<p class="leading-7">${node.content[0].value}</p>`
-            return `<p class="leading-relaxed text-lg tracking-wide text-gray-800">${next(
-              node.content
-            ).replace(/\n/g, '<br/>')}</p>`
-          },
-          [BLOCKS.UL_LIST]: (node, next) => {
-            return `<ul class="list-disc ml-4">
-                        ${next(node.content)}
-                    </ul>`
-          },
-          [BLOCKS.OL_LIST]: (node, next) => {
-            return `<ol class="list-decimal ml-4">
-                    ${next(node.content)}
-                    </ol>`
-          },
-          /* [BLOCKS.LIST_ITEM]: (node) => {
-            node.content = node.content
-              .map((listItemNode) =>
-                listItemNode.nodeType === BLOCKS.PARAGRAPH
-                  ? listItemNode.content
-                  : listItemNode
-              )
-              .flat()
-
-            return `<li class="text-lg leading-relaxed tracking-wide text-gray-800 mt-3">${node.content[0].value}</li>`
-          },
-          [BLOCKS.HR]: () => {
-            return `<div class="border-t border-gray-300 mt-10"></div>`
-          },
-        },
-      }
-    } */
 
     let richText = null
 
-    myHeadings = []
+    headings = []
 
     if (resource.body) {
       richText = documentToHtmlString(
         resource.body.json,
-        // resourceRichTextRenderOptionsx(resource.body.links)
         richTextRenderOptions(currentLocale, resource.body.links, myCallback)
       )
     }
@@ -273,7 +178,6 @@ export default {
       relatedResources,
       headElement,
       headings,
-      myHeadings
     }
   },
 
