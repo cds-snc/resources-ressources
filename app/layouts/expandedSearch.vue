3eb00be3a9483a97f129efb1ed82305b5b94f54b
Dylan <dylanzheng94@users.noreply.github.com>
2023-01-25 11:52:15 -0500

    refactor: remove unused comments

--- app/layouts/expandedSearch.vue
+++ app/layouts/expandedSearch.vue
@@ -4,9 +4,7 @@
     <Header
       class="border-b border-solid border-gray-200 sticky top-0 w-full z-50"
     />
-    <!-- <main class="flex-grow"> -->
-    <!-- <main class="mt-56 md:mt-36"> -->
-    <main >
+    <main>
       <div class="max-w-7xl mx-auto px-5">
         <Nuxt />
       </div>
