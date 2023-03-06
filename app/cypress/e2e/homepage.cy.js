c108037394953e8e8d9ebddab101fd779b8be600
Daine Trinidad <daine.trinidad@cds-snc.ca>
2023-03-06 14:07:21 -0800

    feat: add cypress

---
+++ app/cypress/e2e/homepage.cy.js
@@ -0,0 +1,23 @@
+context('Assertions', () => {
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000') // change URL to match your dev URL
+  // })
+
+  describe('The Home Page', () => {
+
+    it('successfully loads', () => {
+      cy.visit('/') // change URL to match your dev URL
+    })
+  })
+})
+
+/*
+Tests
+- The Home Page
+- Mobile viewports
+  - Navbar etc
+- Contact us
+- About us
+- Terms & Privacy
+ */
+
